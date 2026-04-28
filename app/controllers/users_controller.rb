11d09fdf83f55ac3848f84f66a257d226e80333b
Formasit-chijoh <formasitf@gmail.com>
2026-04-28 18:46:23 +0100

    Add first route and controller method for user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def login
+    email = params[:email]
+    pwd = params[:password]
+    render json: { email:, pwd: }
+  end
+
+  def getname
+    render json: { name: "Hello World" }
+  end
+end
