a4fee49a391f124075505826983b2309717d96e2
Formasit-chijoh <formasitf@gmail.com>
2026-05-01 10:32:37 +0100

    Adds dashboard controller for routing user to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root "sessions#new"
   get  "register"  => "registrations#new"
   post "register"  => "registrations#create"
 
   get    "login"   => "sessions#new"
   post   "login"   => "sessions#create"
   delete "logout"  => "sessions#destroy"
+
+  get "dashboard" => "dashboard#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
