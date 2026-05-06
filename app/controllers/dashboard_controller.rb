68efd7ff4e2895e280bb7cd86b29b37b649417ac
Formasit-chijoh <formasitf@gmail.com>
2026-05-06 12:25:37 +0100

    Structured the layout of the application

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :require_login
 
   def index
-    render current_user.role
+    # render current_user.role
+    @users = User.all
   end
 end
